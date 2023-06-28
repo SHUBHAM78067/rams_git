d61391e49b008e7298d71bc48d007700b2d1c52b
Shubham Sharma <shubham78067@gmail.com>
2023-06-28 19:42:25 +0530

    ok i know every file now

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ramsgit/ramsgit/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"rams_git/rams_git/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
